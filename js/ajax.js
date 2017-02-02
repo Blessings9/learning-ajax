a465ca4f2e9e398930210dea43c04095b39705dd
Blessings9 <bmwafulirwa@gmail.com>
2017-02-02 13:51:10 +0200

    added the Jquery library to the project

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,7 @@ var bio = document.getElementById('bio');
 
 request.open('GET', 'data/ziks.txt');
 request.onreadystatechange = function () {
-  if ((request.status == 200) &&(request.readyState==4)) {
+  if ((request.status == 200) &&(request.readyState == 4)) {
     console.log(request.responseText);
   }
 }
